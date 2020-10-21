2d846bb7118447b22ade59c003c3ac5c8042a8e3
pat-white-code <pat.k.white@gmail.com>
2020-10-21 15:34:22 -0500

    writes route for links response

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const json = {
+  "links": [
+    { "name": "Radio Coffee and Beer", "url": "https://www.radiocoffeeandbeer.com/" },
+    { "name": "SummerMoon Coffee", "url": "https://https://www.patikacoffee.com/" },
+    { "name": "Brew & Brew", "url": "https://www.thebrewandbrew.com/" }
+  ]
+}
+
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
 })
@@ -6,7 +14,13 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
+  if(request.url === '/links') {
+    return new Response(json, {
+      headers: {'Content-Type': 'application/json'}
+    })
+  }
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
 }
+
