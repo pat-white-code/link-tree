c6e606f6939d42e56f10cf378038cabb233761ca
pat-white-code <pat.k.white@gmail.com>
2020-10-21 17:28:55 -0500

    sends links

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-const json = {
-  "links": [
+// const json = require('./links.json');
+// import json from './links.json';
+const data = {
+  "data": [
     { "name": "Radio Coffee and Beer", "url": "https://www.radiocoffeeandbeer.com/" },
     { "name": "SummerMoon Coffee", "url": "https://https://www.patikacoffee.com/" },
     { "name": "Brew & Brew", "url": "https://www.thebrewandbrew.com/" }
   ]
 }
+
+const json = JSON.stringify(data, null, 2)
+const links = /.*links/
 
 addEventListener('fetch', event => {
   event.respondWith(handleRequest(event.request))
@@ -14,11 +19,17 @@ addEventListener('fetch', event => {
  * @param {Request} request
  */
 async function handleRequest(request) {
-  if(request.url === '/links') {
+  if(request.url.match(links)) {
     return new Response(json, {
       headers: {'Content-Type': 'application/json'}
     })
   }
+  console.log(request.url);
+  console.log(json);
+  // return new Response(json, {
+  //   headers: {'Content-Type': 'application/json'}
+  // })
+
   return new Response('Hello worker!', {
     headers: { 'content-type': 'text/plain' },
   })
